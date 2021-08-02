c8b4099f3db62bc2f30b288a2a5a6a1efb3d0510
Monoramen <monoramens@gmail.com>
2021-08-02 10:52:34 +0300

    commit

--- backend/tgclient/admin.py
+++ backend/tgclient/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 import import_export
-from .models import Profile, Message, Product, WarehouseItem
+from .models import ProfileTelegram, Message, Product, WarehouseItem
 from import_export.admin import ImportExportActionModelAdmin
 from import_export import resources
 from import_export import fields
 from import_export.widgets import ForeignKeyWidget
+from django.utils.html import format_html
 # Register your models here.
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'external_id', 'username')
+@admin.register(ProfileTelegram)
+class ProfileTelegramAdmin(admin.ModelAdmin):
+    list_display = ('tg_id', 'tg_username')
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
@@ -45,7 +47,7 @@ class WarehouseItemResource(resources.ModelResource):
 class ProductAdmin(ImportExportActionModelAdmin):
     resource_class = ProductResource
     search_fields = ['name', 'article']
-    list_display = ('name', 'article')
+    list_display = ('name','info', 'article',)
 
 
 @admin.register(WarehouseItem)
@@ -54,4 +56,19 @@ class WarehouseItemAdmin(ImportExportActionModelAdmin):
     autocomplete_fields = ['product']
     search_fields = ['product__name', 'rack']
     list_filter = ('product', 'receipt_date','rack')
-    list_display = ('product', 'quantity', 'status', 'rack', 'receipt_date', 'comments')
+    readonly_fields = ['get_image']
+    list_display = ('product', 'quantity', 'status', 'rack', 'receipt_date', 'comments',)
+        
+    def get_image(self, obj):
+            return format_html('<img src="{}" width="100" height="auto"/>'.format(obj.qr_code.url))
+
+    get_image.short_description = 'Превью'
+    get_image.allow_tags = True
+    def image_data(self, obj):
+        return format_html(
+            '<img src="{}" width="100px"/>',
+            obj.qr_code.path,
+        )
+    image_data.short_description = u'picture'
+    
+    
