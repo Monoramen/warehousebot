b103bb9a930cedc5d9a6233c3053f3ed5e390e7f
Monoramen <monoramens@gmail.com>
2021-08-02 11:00:27 +0300

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,14 @@ USE_TZ = True
 STATIC_URL =  "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 
+
+MEDIA_URL =  "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'media') 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
